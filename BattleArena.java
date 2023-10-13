167d1670aa10bcbb735a145fc073f82ed8ffdc17
Tom Huang <99058458+LilMuh@users.noreply.github.com>
2023-10-13 02:19:24 -0400

    Add files via upload

--- BattleArena.java
+++ BattleArena.java
@@ -3,25 +3,45 @@ package MonsterBattle;
 public class BattleArena {
     public static void main(String[] args) {
         // Default: Health: 50, power: 50
-        Role Brute = new Role("Brute", 100, 75);
-        Role Soldier = new Role("Soldier", 50, 50);
+        Role Brute = new Role("Brute", 100, 80, 15);
+        Role Soldier = new Role("Soldier", 50, 50, 50);
 
+        // Show role info
         Brute.showRoleInfo();
         Soldier.showRoleInfo();
 
-        while(!(die(Soldier)||die(Brute))){
-            Brute.attack(Soldier);
-            Soldier.attack(Brute);
-        }
+        // Two roles battle
+        fight(Brute,Soldier); // Remaining health: Brute -> 0; Soldier -> 2
+        fight(Soldier,Brute); // Remaining health: Brute -> 0; Soldier -> 14
+
     }
 
     public static boolean die(Role role){
         if(role.getHealth()==0){
-            System.out.println();
-            System.out.printf("%s has been killed", role.getName());
             return true;
         }
         return false;
+    }
+
+    public static void fight(Role attacker, Role defender){
+        // two role fight each other
+        while(!(die(defender)||die(attacker))){
+            attacker.attack(defender);
+            // If defender is still alive, attack back
+            if(!die(defender)){defender.attack(attacker);}
+            else{break;}
+        }
+        if(die(defender)){
+            System.out.println();
+            System.out.println(defender.getName()+" has been killed by "+attacker.getName());
+        }
+        else if (die(attacker)) {
+            System.out.println();
+            System.out.println(attacker.getName()+" has been killed by "+defender.getName());
+        }
+        else{
+            System.out.println("Something went wrong, no one died");
+        }
     }
 
 }
--- Role.java
+++ Role.java
@@ -6,6 +6,7 @@ public class Role {
     private String name; // Role name
     private int health; // Role health, 0 to death
     private int power; // Role's power to attack
+    private int armor; // Role's armor to physical damage
 
 
 // Actions
@@ -13,10 +14,11 @@ public class Role {
     public Role() {
     }
 
-    public Role(String name, int health, int power) {
+    public Role(String name, int health, int power, int armor) {
         this.name = name;
         this.health = health;
         this.power = power;
+        this.armor = armor;
     }
 
     public String getName() {
@@ -43,14 +45,30 @@ public class Role {
         this.power = power;
     }
 
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        if(armor>=-100&&armor<=100){
+            this.armor = armor;
+        }
+        else {
+            System.out.println(".....................");
+            System.out.println("The armor is invalid.");
+            System.out.println(".....................");
+        }
+    }
+
     // Design a method to role1.attack(role2)
     public void attack(Role role){
 
-        int remainHealth = role.getHealth() - this.power;
+        int damage = (int)(this.power*(this.armor/100.0));
+        int remainHealth = role.getHealth() - damage;
         remainHealth = remainHealth < 0 ? 0 : remainHealth;
         role.setHealth(remainHealth);
         System.out.println("---------------------------------------------------");
-        System.out.printf("%s hits %s by super power, and it deals %s damage.", this.getName(), role.getName(), this.power);
+        System.out.printf("%s hits %s by super power, and it deals %s damage.", this.getName(), role.getName(), damage);
         System.out.println();
         System.out.printf("%s's remaining health is %s", role.getName(), role.getHealth());
         System.out.println();
@@ -58,12 +76,15 @@ public class Role {
 
     }
 
+
+
     public void showRoleInfo(){
         System.out.println("========================");
         System.out.println("Role info");
         System.out.println("    Name: "+this.getName());
-        System.out.println("    Health: "+this.getHealth());
         System.out.println("    Power: "+this.getPower());
+        System.out.println("    Health: "+this.getHealth());
+        System.out.println("    Armor: "+this.getArmor());
         System.out.println("========================");
     }
 }
